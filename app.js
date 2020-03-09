bd2a54212bea962994922ae98c3fd116753a32d5
Marie Antons <maantons@mail.fhsu.edu>
2020-03-09 15:17:15 -0500

    added photos, started js build

--- app.js
+++ app.js
@@ -2,22 +2,39 @@
 console.log('This is the bus mall');
 
 
- var img1Clicked = 0;
-var img2Clicked = 0;
+var imageElements = document.getElementsByTagName('img');
+
+
+var productIndex1 = 0;
+var productIndex2 = 0;
+var rounds 25;
+var totalProducts = [];
+
+
+//Add constructor function 
+function Advert (name, imageUrl) {
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.timesClicked = 0;
+    totalProducts.push(this);
+}
 
 
 function imageWasClicked() {
+    //track total clicks 
+    totalClicks++;
     console.log('image was clicked');
 
     if (event.srcElement.id === '1') {
         img1Clicked++;
 
     } else if (event.srcElement.id === '2') {
-        img2Clicked++;
+        allImages
+        // img2Clicked++;
     }
 
 
-if(img1Clicked _ img2Clicked >= 5){
+if(img1Clicked + img2Clicked >= 5){
     var footerElement = document.getElementsByTagName('footer')[0];
         footerElement.textContent = `You picked image 1 ${img1Clicked} times and image 2 ${img2Clicked} times.`; /*temporate literal */
     }
